495a679207a5e7b30970e98315447cd35b953f8a
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-11-27 08:26:49 +0100

    [SearchBar] Setup

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -9,11 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    private let barTintColor = UIColor(red: 55/255, green: 120/255, blue: 250/255, alpha: 1.0)
+
+    private let searchBar = UISearchBar()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    // MARK: - Selectors
 
+    @objc func handleShowSearchBar() {
+        search(shouldShow: true)
+        searchBar.becomeFirstResponder()
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        view.backgroundColor = .white
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Search Bar"
+        navigationController?.navigationBar.barTintColor = barTintColor
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.tintColor = .white
+
+        showSearchBarButton(shouldShow: true)
+
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+    }
+
+    private func showSearchBarButton(shouldShow: Bool) {
+        if shouldShow {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search,
+                                                                target: self,
+                                                                action: #selector(handleShowSearchBar))
+        } else {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+
+    private func search(shouldShow: Bool) {
+        showSearchBarButton(shouldShow: !shouldShow)
+        searchBar.showsCancelButton = shouldShow
+        navigationItem.titleView = shouldShow ? searchBar : nil
+
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        search(shouldShow: false)
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("DEBUG: Search bar did begin editing...")
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("DEBUG: Search bar did end editing...")
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("DEBUG: Search text is: \(searchText)")
+    }
 }
 
